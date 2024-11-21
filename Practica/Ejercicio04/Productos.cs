5e10b355c89292b3952a6619c90d1a32be16864a
Wifth <alexander.obando.os2206@gmail.com>
2024-11-21 12:17:36 -0500

    Ejercicio04

---
+++ Practica/Ejercicio04/Productos.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio04
+{
+    internal class Productos
+    {
+        string[,] producto = new string[0, 3];
+        int posicion = 0;
+
+        public void redimensionar(int nuevoTamaño)
+        {
+            string[,] nuevoArreglo = new string[nuevoTamaño, 3];
+            for (int i = 0; i < posicion; i++)
+            {
+                for (int j = 0; j < producto.GetLength(1); j++)
+                {
+                    nuevoArreglo[i, j] = producto[i, j];
+                }
+            }
+            producto = nuevoArreglo;
+        }
+        public void insertar()
+        {
+            redimensionar(posicion + 1);
+            Console.Write("\nIngrese nombre: ");
+            producto[posicion, 0] = Console.ReadLine();
+            Console.Write("Ingrese precio: ");
+            producto[posicion, 0] = Console.ReadLine();
+            Console.Write("Ingrese stock: ");
+            producto[posicion, 0] = Console.ReadLine();
+            posicion++;
+
+            Console.WriteLine("\nProducto registrado correctamente!");
+        }
+        public void mostrar()
+        {
+            for (int i = 0; i <= posicion; i++)
+            {
+                Console.WriteLine("\nN° de producto: " + (i + 1));
+                Console.WriteLine("Producto: " + producto[i,0]);
+                Console.WriteLine("Precio: " + producto[i,1]);
+                Console.WriteLine("Stock: " + producto[i, 2]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Practica/Ejercicio04/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ejercicio04;
+using System.Numerics;
+string regresar;
+Productos p = new Productos();
+
+do
+{
+    Console.WriteLine("****************************************\n");
+    Console.WriteLine("1.       Registrar");
+    Console.WriteLine("2.       Mostrar");
+    Console.WriteLine("3.       Modificar");
+    Console.WriteLine("\n****************************************");
+    Console.Write("\nIngrese una opción: ");
+    int opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1: p.insertar(); break;
+        case 2: p.mostrar(); break;
+        case 3: break;
+    }
+    Console.Write("\nRegresar al menú? [si]: ");
+    regresar = Console.ReadLine();
+    Console.Clear();
+} while (regresar == "si");
