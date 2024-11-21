f8b7e01f52ae9bbdee8f0dd248b4c2981eadf379
Wifth <alexander.obando.os2206@gmail.com>
2024-11-21 09:18:52 -0500

    Ejercicio02

--- Teoria/Ejercicio02/Program.cs
+++ Teoria/Ejercicio02/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+
 int fil = 4;
 int col = 3;
 int[][] mat = new int[fil][];
+int i = 0, j = 0;
 
 //Crea columnas*
 for (int f = 0; f < mat.Length; f++)
@@ -9,10 +12,12 @@ for (int f = 0; f < mat.Length; f++)
     mat[f] = new int[col];
 }
 
+//Ingresa valores del Random*
 Console.Write("Ingrese el valor mínimo: ");
 int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Ingrese el valor máximo");
+Console.Write("Ingrese el valor máximo: ");
 int b = int.Parse(Console.ReadLine());
+Console.WriteLine();
 
 //Ingrreso números*
 for (int f = 0; f < mat.Length; f++)
@@ -29,7 +34,26 @@ for (int f = 0; f < mat.Length; f++)
 {
     for (int c = 0; c < mat[0].Length; c++)
     {
-        Console.Write($"{mat[f][c]}, ");
+        Console.Write($"{mat[f][c]}    ");
     }
     Console.WriteLine();
+}
+
+//Sumatoria de filas y colummnas*
+for (int f = 0; f < mat.Length; f++)
+{
+    for (int c = 0; c < mat[0].Length; c++)
+    {
+        i += mat[f][c];
+    }
+    Console.Write($"\nSuma en fila {f+1}:    " + i);
+}
+Console.WriteLine();
+for (int c = 0; c < mat[0].Length; c++)
+{
+    for (int f = 0; f < mat.Length; f++)
+    {
+        j += mat[f][c];
+    }
+    Console.Write($"\nSuma en columna {c+1}: " + j);
 }
